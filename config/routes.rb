25bce291c971a2ffefd5e42f9ab5d44950724ee6
Jason McLean <jasonamclean@hotmail.com>
2016-12-06 14:14:04 -0700

    Setting up controllers and view pages

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+	resources :posts
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+  # get 'new', to: 'posts#new'
+
+  # get 'show', to: 'posts#show'
+
+  # get 'edit', to: 'posts#edit'
+
+  # root 'posts#index'
